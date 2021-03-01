99d8ccada5d2cf37d54a3e61cd48b777b3517e92
Joshua Byrd <phocks@gmail.com>
2021-03-01 16:40:55 +1000

    Put a box in

--- snowpack.config.js
+++ snowpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     public: { url: "/", static: true },
     src: { url: "/dist" },
   },
-  plugins: ["@snowpack/plugin-svelte", "@snowpack/plugin-dotenv"],
+  plugins: ["@snowpack/plugin-dotenv", "@snowpack/plugin-svelte"],
   routes: [
     /* Enable an SPA Fallback in development: */
     // {"match": "routes", "src": ".*", "dest": "/index.html"},
--- src/App.svelte
+++ src/App.svelte
@@ -4,6 +4,8 @@
   import { setClient, query } from "svelte-apollo";
   import { gql } from "@apollo/client";
 
+  let inputText = "";
+
   const client = new ApolloClient({
     uri: "https://guestbook-api.phocks.org/apollo",
     cache: new InMemoryCache(),
@@ -22,15 +24,26 @@
 
   onMount(() => {
     // Do stuff on mount
+    console.log(":)");
     return () => {
       // Do stuff on unmount
     };
   });
+
+  function postText(event) {
+    // console.log(event);
+    console.log(inputText);
+  }
 </script>
 
 <div class="App">
+  <form on:submit|preventDefault={postText}>
+    <input bind:value={inputText} placeholder="Enter a message" required />
+    <button type="submit">Post</button>
+  </form>
+
   {#if $posts.loading}
-    Loading...
+    <!-- Loading... -->
   {:else}
     <ul>
       {#each $posts.data.posts as post}
@@ -43,7 +56,39 @@
 <style>
   :global(body) {
     font-size: 16px;
-	  margin: 0;
-	  font-family: Arial, Helvetica, sans-serif;
-	}
+    margin: 20px;
+    font-family: "Work Sans", sans-serif;
+    color: #333;
+  }
+
+  .App input {
+    margin-bottom: 2em;
+    font-size: 16px;
+    font-size: max(16px, 1em);
+    font-family: inherit;
+    padding: 0.25em 0.5em;
+    background-color: #fff;
+    border: 1px solid rgb(139, 139, 139);
+    border-radius: 0px;
+  }
+
+  .App button {
+    font-size: 16px;
+    font-size: max(16px, 1em);
+    font-family: inherit;
+    background-color: transparent;
+    border: 1px solid rgb(139, 139, 139);
+    border-radius: 0px;
+    padding: 0.25em 0.5em;
+  }
+
+  .App ul {
+    list-style: none;
+    margin: 0;
+    padding: 0;
+  }
+
+  .App ul li {
+    margin-bottom: 1.6em;
+  }
 </style>
